d10f7d8c29018225b9bf751af14269de93db44a6
Amanda Shen <amandashen@Amandas-MBP.fios-router.home>
2023-01-25 07:51:01 -0500

    add alt.py

---
+++ albumy/blueprints/alternative.py
@@ -0,0 +1,23 @@
+import json
+
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes, VisualFeatureTypes
+
+def get_alternative_text(local_path:str):
+    secrete = json.load(open('env/secrete.json'))
+    API_KEY = secrete['API_KEY']
+    END_POINT = secrete['END_POINT']
+    client = ComputerVisionClient(END_POINT,CognitiveServicesCredentials(API_KEY))
+    try:
+        respond = client.describe_image_in_stream(
+        open(local_path,'rb'),
+        raw=True
+        )
+        text = respond.output.captions[0].text
+    except:
+        print("Error occur when fetching alternating text from azure")
+
+    return text
+
+
